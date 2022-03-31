7a0f718b8e26a0e21969d6008897081ffe1ec2d9
Fanqi Yu <yu_fanqi@163.com>
2022-03-31 16:36:14 +0800

    Use conv kernel as test case

--- tests/dev/dev_run.py
+++ tests/dev/dev_run.py
@@ -1,8 +1,27 @@
 from akg.ops.math.add import Add
+from akg.ops.nn.gpu.conv import Conv
 from akg.utils import kernel_exec as utils
 
-def test_ms_add(shape1, shape2, dtype):
-    mod = utils.op_build_test(Add, (shape1, shape2), (dtype, dtype), kernel_name="add", attrs={"target": "cuda"})    
+from tests.common.test_run import conv_run
+
+
+def test_ms_conv(shape1, shape2, dtype):
+    mod = utils.op_build_test(Conv, (shape1, shape2), (dtype, dtype), kernel_name="Conv", attrs={"target": "cuda"})
+    return mod
 
 if __name__ == '__main__':
-    test_ms_add((1, 1024, 512), (1, 1024, 512), 'float32')
+    args_default = [
+        ("000_case", conv_run, ((32, 64, 56, 56), (64, 64, 3, 3), (1, 1), (1, 1, 1, 1), (1,1), "float32", "float32", "NCHW"), ["level0"]),
+        ("001_case", conv_run, ((16, 4, 4, 16), (16, 3, 3, 16), (1, 1), (0, 0, 0, 0), (1, 1), "float16", "float16"), ["level0"]),
+        ("002_case", conv_run, ((64, 6, 6, 64), (64, 3, 3, 64), (1, 1), (0, 0, 0, 0), (1, 1), "float16", "float32"), ["level0"]),
+        ("003_case", conv_run, ((64, 6, 6, 64), (64, 3, 3, 64), (1, 1), (0, 0, 0, 0), (1, 1), "float16", "float16"), ["level0"]),
+    ]
+    
+    attrs = {}
+    attrs["target"] = utils.CUDA
+    attrs["profiling"] = False
+    attrs["repeat_times"] = 1000
+    
+    # conv_run((32, 64, 56, 56), (64, 64, 3, 3), (1, 1), (1, 1, 1, 1), (1, 1), "float32", "float32", "NCHW", attrs=attrs)
+    conv_run((16, 4, 4, 16), (16, 3, 3, 16), (1, 1), (0, 0, 0, 0), (1, 1), "float16", "float16", "NHWC", attrs=attrs)
+    
