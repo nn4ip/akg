3d86b0bafc9e8e442a0e517ecf9052745b30d5af
Fanqi Yu <yu_fanqi@163.com>
2022-04-30 13:27:51 +0800

    Update dev_run.py

--- tests/dev/dev_run.py
+++ tests/dev/dev_run.py
@@ -10,7 +10,7 @@ def test_ms_conv(shape1, shape2, dtype):
     return mod
 
 if __name__ == '__main__':
-    args_default = [
+    args_default = [ # (batch, in_c, in_h, in_w), (out_c, in_c, k_h, k_w), stride, pad, dilation
         ("000_case", conv_run, ((32, 64, 56, 56), (64, 64, 3, 3), (1, 1), (1, 1, 1, 1), (1,1), "float32", "float32", "NCHW"), ["level0"]),
         ("001_case", conv_run, ((16, 4, 4, 16), (16, 3, 3, 16), (1, 1), (0, 0, 0, 0), (1, 1), "float16", "float16"), ["level0"]),
         ("002_case", conv_run, ((64, 6, 6, 64), (64, 3, 3, 64), (1, 1), (0, 0, 0, 0), (1, 1), "float16", "float32"), ["level0"]),
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     attrs["profiling"] = False
     attrs["repeat_times"] = 1000
     
-    # conv_run((32, 64, 56, 56), (64, 64, 3, 3), (1, 1), (1, 1, 1, 1), (1, 1), "float32", "float32", "NCHW", attrs=attrs)
-    conv_run((16, 4, 4, 16), (16, 3, 3, 16), (1, 1), (0, 0, 0, 0), (1, 1), "float16", "float16", "NHWC", attrs=attrs)
+    conv_run((32, 64, 56, 56), (64, 64, 3, 3), (1, 1), (1, 1, 1, 1), (1, 1), "float32", "float32", "NCHW", attrs=attrs)
+    # conv_run((16, 4, 4, 16), (16, 3, 3, 16), (1, 1), (0, 0, 0, 0), (1, 1), "float16", "float16", "NHWC", attrs=attrs)
+    # conv_run((16, 4, 300, 300), (16, 4, 3, 3), (1, 1), (0, 0, 0, 0), (1, 1), "float16", "float16", "NHWC", attrs=attrs)
     
